2e4390060f7c15b682fe01bd414a99cb3052f743
jordan-bott <jordanbott.dev@gmail.com>
2024-02-15 23:07:28 -0800

    meal box formatting, remove from meal box

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -2,16 +2,25 @@ import { useState, Fragment } from "react";
 import foodList from "./data/foods";
 import FoodBox from "./components/FoodBox";
 import title from "./assets/title.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [mealList, setMealList] = useState([]);
 
   const addFoodToMeal = (food) => {
     if (mealList.includes(food)) {
-      <p>{`Uh oh you added ${food} already!`}</p>;
+      toast(`Uh oh, you already added ${food} to your list!`, {
+        position: "bottom-right",
+      });
     } else {
       setMealList([...mealList, food]);
     }
+  };
+
+  const removeFoodFromMeal = (food) => {
+    const updatedList = mealList.filter((item) => item !== food);
+    setMealList(updatedList);
   };
 
   return (
@@ -24,9 +33,17 @@ function App() {
           </p>
         </div>
         <div className="w-[40%] h-100">
-          <div className="border-grey border-4 bg-lgrey  rounded-xl w-[80%] h-[80%] mt-8 ml-4">
+          <div className="border-grey border-4 bg-lgrey rounded-xl w-[80%] h-[80%] mt-8 ml-4 columns-4 p-2 content-around">
             {mealList.map((food) => {
-              return <p key={food}>{food}</p>;
+              return (
+                <p
+                  className="hover:line-through"
+                  key={food}
+                  onClick={() => removeFoodFromMeal(food)}
+                >
+                  {food}
+                </p>
+              );
             })}
           </div>
         </div>
@@ -40,6 +57,7 @@ function App() {
           );
         })}
       </div>
+      <ToastContainer />
     </div>
   );
 }
