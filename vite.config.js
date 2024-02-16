8c46b36c475137e34dac6ff9bba74b711ef31025
jordan-bott <jordanbott.dev@gmail.com>
2024-02-16 15:07:41 -0800

    workflow updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/<REPO>/",
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "~", replacement: path.resolve(__dirname, "node_modules") },
+    ],
+  },
 });
