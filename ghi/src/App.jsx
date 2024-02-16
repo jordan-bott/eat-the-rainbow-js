120f6900c9df9403637450cc961bd45633b07f63
jordan-bott <jordanbott.dev@gmail.com>
2024-02-15 21:05:33 -0800

    adding meal list

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,22 +1,46 @@
 import { useState, Fragment } from "react";
 import foodList from "./data/foods";
 import FoodBox from "./components/FoodBox";
+import title from "./assets/title.png";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [mealList, setMealList] = useState([]);
+
+  const addFoodToMeal = (food) => {
+    if (mealList.includes(food)) {
+      <p>{`Uh oh you added ${food} already!`}</p>;
+    } else {
+      setMealList([...mealList, food]);
+    }
+  };
 
   return (
-    <>
-      <p> Hi</p>
-      {foodList.map((food) => {
-        return (
-          <Fragment key={food.color}>
-            <p>{food.color}</p>
-            <FoodBox {...food} />
-          </Fragment>
-        );
-      })}
-    </>
+    <div className="h-screen w-screen">
+      <div className="flex w-100">
+        <div className="w-[60%]">
+          <img src={title} className="w-[70%] mx-8 mt-4" />
+          <p className="pl-20 text-xl">
+            Click on the foods you ate today below
+          </p>
+        </div>
+        <div className="w-[40%] h-100">
+          <div className="border-grey border-4 bg-lgrey  rounded-xl w-[80%] h-[80%] mt-8 ml-4">
+            {mealList.map((food) => {
+              return <p key={food}>{food}</p>;
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row place-content-center gap-x-2">
+        {foodList.map((food) => {
+          return (
+            <Fragment key={food.color}>
+              <FoodBox {...food} addFoodToMeal={addFoodToMeal} />
+            </Fragment>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
--- ghi/src/components/FoodBox.jsx
+++ ghi/src/components/FoodBox.jsx
@@ -1,10 +1,28 @@
 /* eslint react/prop-types: 0 */
-export default function FoodBox({ background, border, highlight }) {
+export default function FoodBox({
+  background,
+  border,
+  highlight,
+  foods,
+  addFoodToMeal,
+}) {
   return (
     <div
       className={`flex flex-col ${background} ${border} border-4 w-[15%] my-8 h-[70vh] rounded-xl`}
     >
-      <p>{highlight}</p>
+      <div className="my-8 text-center text-lg">
+        {foods.map((food) => {
+          return (
+            <ul
+              className={`px-3 ${highlight}`}
+              key={food}
+              onClick={() => addFoodToMeal(food)}
+            >
+              {food}
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
