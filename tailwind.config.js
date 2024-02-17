9a8647f4e5bc4a57a811c1d00bebe0f32c7a9efa
jordan-bott <jordanbott.dev@gmail.com>
2024-02-17 01:00:57 -0800

    removes tailwind-highlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,8 +24,5 @@ export default {
         "linear-gradient(to right, #FFADAD, #FFBF70, #FDEF5E, #B1F84F, #A0C4FF, #BDB2FF)",
     },
   },
-  plugins: [
-    require("tailwind-scrollbar")({ nocompatible: true }),
-    require("tailwindcss-highlights"),
-  ],
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
