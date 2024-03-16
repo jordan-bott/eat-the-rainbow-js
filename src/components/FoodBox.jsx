c55f3085e1617c86cd0f854e798e2f9af85a7b51
jordan-bott <jordanbott.dev@gmail.com>
2024-03-16 15:02:27 -0700

    fix scrollbars

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -14,7 +14,7 @@ export default function FoodBox({
       className={`flex flex-col ${background} ${border}  border-4 w-[15%] mt-8 h-[64vh] rounded-xl`}
     >
       <div
-        className={`my-6 text-center text-lg flex flex-col overflow-scroll scrollbar-thin scrollbar-thumb-rounded-lg`}
+        className={`my-6 text-center text-lg flex flex-col overflow-auto scrollbar-thin scrollbar-thumb-rounded-lg`}
       >
         {foods.map((food) => {
           let foodInList = null;
--- src/components/MealBox.jsx
+++ src/components/MealBox.jsx
@@ -1,7 +1,7 @@
 /* eslint react/prop-types: 0 */
 export default function MealBox({ mealList, removeFoodFromMeal }) {
   return (
-    <div className="flex flex-wrap border-grey border-4 bg-lgrey rounded-xl w-[80%] h-[180px] mt-8 ml-4 columns-4 p-2 overflow-scroll scrollbar-thin scrollbar-thumb-rainbow scrollbar-thumb-rounded-lg">
+    <div className="flex flex-wrap border-grey border-4 bg-lgrey rounded-xl w-[80%] h-[180px] mt-8 ml-4 columns-4 p-2 overflow-auto scrollbar-thin scrollbar-thumb-rounded-lg">
       {mealList.map((food) => {
         return (
           <span
